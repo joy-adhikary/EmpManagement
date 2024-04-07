40709dae63027d8f94413d8763e3734802c2a0e9
joy-adhikary <joyadhikary074@gmail.com>
2024-04-07 21:24:56 +0600

    admin crud with serializer added

--- Adm/views.py
+++ Adm/views.py
@@ -28,7 +28,7 @@ def admIndex(request):
         # getting put data from front end
         post_content = request.data
 
-        print("Yho, you heat Post method from admin")
+        print("Yho, you hit Post method from admin")
         print(post_content)
         return Response(post_content)
 
@@ -37,13 +37,12 @@ def admIndex(request):
 
 #  standard methods to CRUD datas
 
-
-@api_view(['GET', 'POST', 'PUT', 'PATCH'])
+@api_view(['GET', 'POST', 'PUT', 'PATCH', 'DELETE'])
 
 def admStdIndex(request):
     if request.method == 'GET':
         data = Admin.objects.all()
-        print("all data", data)
+        # print("all data", data)
         serialized_data = AdminSerializers(data, many=True)
         return Response(serialized_data.data)
 
@@ -64,22 +63,36 @@ def admStdIndex(request):
     elif request.method == 'PUT':
         Put_content = request.data
         print("PUT content =>", Put_content)
-        # id = Put_content['id']
-        serialized_data = AdminSerializers(data = Put_content)
+
+        updatedObj = Admin.objects.get(id= Put_content['id'])
+        serialized_data = AdminSerializers(updatedObj,data = Put_content)
+
         if serialized_data.is_valid():
             serialized_data.save()
             return Response(serialized_data.data)
 
         return Response({"Message": serialized_data.errors}, status=status.HTTP_406_NOT_ACCEPTABLE)
 
+
+    # patch a pertial update kora jai.Baki data as it is rekhe dibe. just passed data gulai update kore dibe.
 
     elif request.method == "PATCH":
         patch_content = request.data
         print("PATCH content =>", patch_content)
-        serialized_data = AdminSerializers(data=patch_content, partial=True)
-        print("PATCH info ", patch_content)
+
+        updatedObj = Admin.objects.get(id = patch_content['id'])
+        serialized_data = AdminSerializers(updatedObj,data = patch_content, partial = True)
+        print("Updated object", updatedObj)
+
         if serialized_data.is_valid():
             serialized_data.save()
             return Response(serialized_data.data)
 
         return Response({"Message": serialized_data.errors}, status=status.HTTP_406_NOT_ACCEPTABLE)
+    
+    #  delete method 
+    else:
+        request_data = request.data 
+        updatedObj = Admin.objects.get(id=request_data['id'])
+        updatedObj.delete()
+        return Response({"Message": "Entity deleted :)"}, status=status.HTTP_200_OK)
