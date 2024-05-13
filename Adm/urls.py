9049c4e3f31b568c801cf710c31f6b0bfedbef49
joy-adhikary <joyadhikary074@gmail.com>
2024-05-13 09:32:57 +0600

    Class API view added in both admin and emp

--- Adm/urls.py
+++ Adm/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from Adm.views import admStdIndex
+from Adm.views import admStdIndex,AdminClass
 
 urlpatterns = [
-    path("AdmSeri/",admStdIndex)
+    # path("AdmSeri/",admStdIndex)
+    path("AdmSeri/",AdminClass.as_view())
 ]
